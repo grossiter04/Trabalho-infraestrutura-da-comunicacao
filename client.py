f2cff9eb3c99ff317a6c0ce725a40cb925c0525c
dan_melo <114592376+dan-albuquerque@users.noreply.github.com>
2024-03-23 11:46:52 -0300

    feita soma de verificacao

--- client.py
+++ client.py
@@ -1,5 +1,17 @@
 from socket import *
+import pickle
 import time
+
+# Função para calcular a soma de verificação
+def checksum(data):
+    # Converter todas as strings em bytes antes de calcular a soma
+    data_bytes = [bytes(element, 'utf-8') if isinstance(element, str) else bytes([element]) for element in data]
+    # Somar os bytes usando operadores binários
+    result = 0
+    for byte_data in data_bytes:
+        result += int.from_bytes(byte_data, byteorder='big')
+    return result & 0xFFFFFFFF
+
 
 serverName = "localhost"
 serverPort = 12000
@@ -7,12 +19,16 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 while True:
     message = input("Enter a lowercase sentence: ")
+    
+    message_checksum = checksum(message.encode())
 
-    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    data_to_send = [message, message_checksum]
+    clientSocket.sendto(pickle.dumps(data_to_send), (serverName, serverPort))
 
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    response_data, serverAddress = clientSocket.recvfrom(2048)
 
-    print(modifiedMessage.decode())
+    # Separa a mensagem e a soma de verificação
+    modifiedMessage, checksum_received = pickle.loads(response_data)
+    print(modifiedMessage)
+
     time.sleep(1)
-
-# clientSocket.close()
